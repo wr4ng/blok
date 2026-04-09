c8aa63f961acfc33739ebfcee40630f5efa7c987
Mads Wrang <madscwn@gmail.com>
2026-04-09 16:34:26 +0200

    move run() function to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,32 @@
 #![warn(clippy::pedantic)]
 #![allow(clippy::must_use_candidate)]
 
-pub mod cli;
-pub mod duration;
-pub mod entry;
+mod cli;
+mod duration;
+mod entry;
 
-pub use entry::Entry;
+pub use cli::Cli;
+use cli::Command;
+
+pub fn run(cli: Cli) {
+    match &cli.command {
+        Command::Print {
+            tag: _,
+            from: _,
+            to: _,
+        } => todo!("not implemented"),
+        Command::Report {
+            group_by: _,
+            from: _,
+            to: _,
+            tag: _,
+            include_empty: _,
+        } => todo!("not implemented"),
+        Command::Add {
+            date: _,
+            duration: _,
+            tag: _,
+            note: _,
+        } => todo!("not implemented"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,7 @@
-use blok::cli::{Cli, Command};
+use blok::Cli;
 use clap::Parser;
 
 fn main() {
     let cli = Cli::parse();
-
-    //TODO: Have a single "run" function that takes the parsed CLI and does the work, so we can test it without invoking the CLI parser.
-    match &cli.command {
-        Command::Print {
-            tag: _,
-            from: _,
-            to: _,
-        } => todo!("not implemented"),
-        Command::Report {
-            group_by: _,
-            from: _,
-            to: _,
-            tag: _,
-            include_empty: _,
-        } => todo!("not implemented"),
-        Command::Add {
-            date: _,
-            duration: _,
-            tag: _,
-            note: _,
-        } => todo!("not implemented"),
-    }
+    blok::run(cli);
 }
