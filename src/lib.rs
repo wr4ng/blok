d1d633e0160fa432bd97c5b85b4badf4db6c5460
Mads Wrang <madscwn@gmail.com>
2026-04-09 17:43:53 +0200

    implement add command

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,19 @@ mod cli;
 mod duration;
 mod entry;
 
+use std::fs::OpenOptions;
+use std::io::Write;
+
 pub use cli::Cli;
 use cli::Command;
 use entry::Entry;
 
 pub fn run(cli: Cli) {
+    if !cli.file.exists() {
+        eprintln!("blok file '{}' does not exist", cli.file.display());
+        return;
+    }
+
     match cli.command {
         Command::Add {
             date,
@@ -19,7 +27,23 @@ pub fn run(cli: Cli) {
         } => {
             let date = date.unwrap_or_else(|| jiff::Zoned::now().date());
             let entry = Entry::new(date, duration, tags.0, note);
-            println!("{entry}");
+
+            let mut file = match OpenOptions::new().append(true).open(&cli.file) {
+                Ok(file) => file,
+                Err(e) => {
+                    eprintln!("failed to open '{}': {}", cli.file.display(), e.kind());
+                    return;
+                }
+            };
+            if let Err(e) = writeln!(file, "{entry}") {
+                eprintln!(
+                    "failed to write new entry to '{}': {}",
+                    cli.file.display(),
+                    e.kind()
+                );
+                return;
+            }
+            println!("Entry added:\n{entry}");
         }
         Command::Print {
             tag: _,
